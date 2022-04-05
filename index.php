6c3f8375083822faf32374de44650dd723859096
hnganh367 <hnganh367@gmail.com>
2022-04-05 11:08:11 +0700

    Added Link To Heroku

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ include 'includes/header.php' ?>
     <button type="button" class="btn btn-dark">CLICK ME!</button>
     <button type="button" class="btn btn-primary">CLICK ME!</button>
     <button type="button" class="btn btn-success">CLICK ME!</button>
-
+    <a href="https://www.heroku.com" class="btn btn-danger" target="_blank">Heroku.com</a>
     <?php 
         require 'includes/footer.php'
     ?>
